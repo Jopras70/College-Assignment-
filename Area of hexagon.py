d6a5a71806f3f76e89d29c0e190aa6f9b881ed35
Jonathan Prasetyo <91464711+Jopras70@users.noreply.github.com>
2021-10-29 00:30:37 +0700

    Delete Area of hexagon.py

--- Area of hexagon.py
+++
@@ -1,8 +0,0 @@
-import math 
-
-sidelength = eval(input("Enter the side length of the hexagon: ")) 
-
-area = (3*math.sqrt(3)/2)*(math.pow(sidelength,2))
-
-print (f"The area of a hexagon with side length {sidelength} is {format('%.3f'%area)}")
-
