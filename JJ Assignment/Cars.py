89619219e575a93f29e2954f435f5162e33b35ac
Jonathan Prasetyo <91464711+Jopras70@users.noreply.github.com>
2021-11-06 01:08:34 +0700

    Add files via upload

---
+++ JJ Assignment/Cars.py
@@ -0,0 +1,14 @@
+dict = { 
+    "farrel": ['audi','jazz','civic'],
+    "ivan": ['bmw','fortuner','brio'],
+    "robin": ['mustang','ertiga','mercy'],
+    "yusuf": ['rubicon','mazda','pajero'],
+    "joshua": ['innova','porsche','city']
+}
+
+for x in dict: 
+    print (x, 'Likes this car: ')
+    for y in dict [x]: 
+        print ('-',y)
+
+        
