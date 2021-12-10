87e87385349191deb0ad09b411727b3710350723
Jonathan Prasetyo <91464711+Jopras70@users.noreply.github.com>
2021-12-11 02:57:22 +0700

    Add files via upload

---
+++ JJ Assignment/Classes1.py
@@ -0,0 +1,26 @@
+# Demonstration of MRO
+
+class X:
+    pass
+
+
+class Y:
+    pass
+
+
+class Z:
+    pass
+
+
+class A(X, Y):
+    pass
+
+
+class B(Y, Z):
+    pass
+
+
+class M(B, A, Z):
+    pass
+
+print(M.mro())
